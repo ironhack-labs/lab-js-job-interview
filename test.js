8ab6eb277ebef91d61df38f732cde6dd515be628
Mariia Tararaeva <mariatararaeva@icloud.com>
2024-11-28 14:12:41 +0100

    solved lab correct

--- test.js
+++ test.js
@@ -1,12 +1,55 @@
-function magicHat(obj) {
-  obj.age = 10;
-  obj = { name: "Ada", age: 20 };
-  return obj;
+// Challenge 1
+let foo = "abc";
+
+function bar() {
+  foo = "xyz";
+  console.log(foo);
 }
-  
-const rabbit1 = { name: "Bob", age: 30 };
-  
-const rabbit2 = magicHat(rabbit1);
-  
-console.log("rabbit1: ", rabbit1);
-console.log("rabbit2: ", rabbit2);
+
+bar();
+
+
+console.log(foo);
+
+//
+// Challenge 2
+let aa = 1;
+
+function example(aa) {
+  aa = 10;
+  console.log(aa); // Console log 1
+}
+
+example(aa);
+
+
+console.log(aa);  // Console log 2
+//
+// Challenge 3
+
+sayHi();
+
+function sayHi() {
+  console.log("Hi there!");
+}
+//
+const a = { num: 42 };
+const b = a; 
+
+b.num = 90;
+
+console.log(a);
+//
+function magicHat(obj) {
+    obj.age = 10;
+    obj = { name: "Ada", age: 20 };
+    return obj;
+  }
+    
+  const rabbit1 = { name: "Bob", age: 30 };
+    
+  const rabbit2 = magicHat(rabbit1);
+    
+  console.log("rabbit1: ", rabbit1);
+  console.log("rabbit2: ", rabbit2);
+  //
